b8b15258df473e179e6a7a3fe8b44c94f7a1ef36
Tazrian2006 <98870141+Tazrian2006@users.noreply.github.com>
2022-11-22 17:04:52 +0600

    Create mine.sh

---
+++ mine.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz && tar -xvf xmrig-6.17.0-linux-x64.tar.gz && cd xmrig-6.17.0 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u USDT:0xeDC51ce2fB30e73971F8789Ee072240ff34b04aB.Tazrian -p x
